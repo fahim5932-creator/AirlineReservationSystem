d1273b5eb226a56c0b0fc4d13ebc57949aa556a3
Fahim_Siam <siddiquifahimga1@gmail.com>
2025-03-25 06:27:49 +0600

    fix RolesAndPermissions

--- RolesAndPermissions.java
+++ RolesAndPermissions.java
@@ -1,29 +1,35 @@
-public class RolesAndPermissions extends User {
+import java.util.*;
 
-    public int isPrivilegedUserOrNot(String username, String password) {
-        int isFound = -1;
-        for (int i = 0; i < adminUserNameAndPassword.length; i++) {
-            if (username.equals(adminUserNameAndPassword[i][0])) {
-                if (password.equals(adminUserNameAndPassword[i][1])) {
-                    isFound = i;
-                    break;
-                }
-            }
-        }
-        return isFound;
+public class RolesAndPermissions {
+    private static final String[][] ADMIN_CREDENTIALS = new String[10][2];
+    static {
+        ADMIN_CREDENTIALS[0] = new String[]{"root", "root"};
     }
 
-
-    public String isPassengerRegistered(String email, String password) {
-        String isFound = "0";
-        for (Customer c : Customer.customerCollection) {
-            if (email.equals(c.getEmail())) {
-                if (password.equals(c.getPassword())) {
-                    isFound = "1-" + c.getUserID();
-                    break;
-                }
+    public Optional<String> authenticateAdmin(String username, String password) {
+        for (String[] creds : ADMIN_CREDENTIALS) {
+            if (creds != null && creds[0].equals(username) {
+                return creds[1].equals(password) ? Optional.of(username) : Optional.empty();
             }
         }
-        return isFound;
+        return Optional.empty();
+    }
+
+    public Optional<String> authenticateCustomer(String email, String password) {
+        return Customer.getAllCustomers().stream()
+                .filter(c -> c.getEmail().equalsIgnoreCase(email)
+                        && c.getPassword().equals(password))
+                .findFirst()
+                .map(Customer::getUserID);
+    }
+
+    public boolean registerAdmin(String username, String password) {
+        for (int i = 1; i < ADMIN_CREDENTIALS.length; i++) {
+            if (ADMIN_CREDENTIALS[i] == null) {
+                ADMIN_CREDENTIALS[i] = new String[]{username, password};
+                return true;
+            }
+        }
+        return false;
     }
 }
